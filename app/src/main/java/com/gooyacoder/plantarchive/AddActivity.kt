d5c93f7473e228aa79016004b078b8dd56f843ef
amirhosein <gooyacoder@gmail.com>
2025-02-05 19:54:24 +0330

    17_11_1403    19:54

--- app/src/main/java/com/gooyacoder/plantarchive/AddActivity.kt
+++ app/src/main/java/com/gooyacoder/plantarchive/AddActivity.kt
@@ -1,29 +1,20 @@
 package com.gooyacoder.plantarchive
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class MainActivity : AppCompatActivity() {
+class AddActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_add)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val addButton: FloatingActionButton = findViewById(R.id.addPlantButton)
-        addButton.setOnClickListener(View.OnClickListener {
-            // Show addPlantActivity
-            val addActivity = Intent(this, AddActivity::class.java)
-            startActivity(addActivity)
-        })
     }
 }
