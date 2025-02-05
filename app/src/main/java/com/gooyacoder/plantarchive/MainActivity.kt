c2ae987e2543da077ca67601cf09f12b3c267e84
amirhosein <gooyacoder@gmail.com>
2025-02-05 12:38:01 +0330

    17_11_1403    12:37

--- app/src/main/java/com/gooyacoder/plantarchive/MainActivity.kt
+++ app/src/main/java/com/gooyacoder/plantarchive/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.gooyacoder.plantarchive
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val addButton: FloatingActionButton = findViewById(R.id.addPlantButton)
+        addButton.setOnClickListener(View.OnClickListener {
+            // Show addPlantActivity
+
+        })
     }
 }
